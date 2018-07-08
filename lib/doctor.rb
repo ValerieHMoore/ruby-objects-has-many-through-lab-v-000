6cd95f76b248666d358ecf99f3f160ca9ea1b474
Valerie Moore <periwinkle17@outlook.com>
2018-07-08 16:04:00 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,13 +4,13 @@ class Doctor
   attr_accessor :name
   @@all = []
   
-  def self.all
-    @@all
-  end
-  
   def initialize(name)
     @name = name
     @@all << self
+  end
+  
+  def self.all
+    @@all
   end
   
   def new_appointment(date, patient)
