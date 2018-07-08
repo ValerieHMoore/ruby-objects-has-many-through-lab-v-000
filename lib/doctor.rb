7e97d32f78014e7003f830edc938fe61f10c33bc
Valerie Moore <periwinkle17@outlook.com>
2018-07-08 16:27:34 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,14 @@ class Doctor
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
-  def patients
+  # describe "#patients" do
+  #   it "has many patients, through appointments" do
+  #     doctor_who = Doctor.new("The Doctor")
+  #     hevydevy = Patient.new("Devin Townsend")
+  #     doctor_who.new_appointment(hevydevy, "Friday, January 32nd")
+  #     expect(doctor_who.patients).to include(hevydevy)
+
+    def patients
     Appointment.all.collect {|appointment| appointment.patient}
   end
   
