81e84b496675361bcbbbade131761a08e5dc1e8c
Valerie Moore <periwinkle17@outlook.com>
2018-07-08 16:28:20 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -28,7 +28,7 @@ class Doctor
     Appointment.all.collect {|appointment| appointment.patient}
   end
   
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     Appointment.new(date, patient, self)
   end
   
